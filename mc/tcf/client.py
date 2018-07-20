6fcb5b37434e4a8e7161f8b6e44e20a0c3f2da13
Matteo Casadei <matteo.casadei@gmail.com>
2018-07-20 23:35:21 +0200

    Parametric load of model

--- mc/tcf/client.py
+++ mc/tcf/client.py
@@ -7,7 +7,8 @@ class MLClient:
 
     def __init__(self, config):
         self.config = config
-        self.ensemble = self._load_model()
+        if self.config.app_config['load_model']:
+            self.ensemble = self._load_model()
 
     def _load_model(self):
         ml_model_path = os.path.join(self.config.app_config['base_dir'], self.config.app_config['ml_trained_model'])
--- mc/tcf/config.py
+++ mc/tcf/config.py
@@ -8,6 +8,7 @@ messages = {
 }
 
 app_config = {
+    "load_model": os.environ.get('LOAD_MODEL', False),
     "ml_trained_model": "ensemble_rf_estimator.pkl",
     "base_dir": "ml"
 }
